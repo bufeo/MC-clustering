d012b777df24603c99656f02c9cd2a5ed5588472
Julia Stadler <julia.j.stadler@durham.ac.uk>
2017-11-27 19:39:01 +0000

    The norm function can now: * write all ressults to an output loc file (.txt format) * return the combined norm from all data sets

--- mc_clustering/analyse_norm.py
+++ mc_clustering/analyse_norm.py
@@ -13,24 +13,41 @@ def analyse_norm(in_dir, out_dir, out_file):
     
     sim_dat = {}
     sims = []
-    
-    print('\nincluding following files:')
+    out_txt = out_file + '.txt'
+    with open(out_txt, 'w') as f:
+        f.write('ANALYZING MC NORMALISATION\n\n')
+        f.write('including the following files:\n')
+        print('\nincluding following files:')
 
-    for i in range(0,len(sim_list)):
-        print(' -> %s'%(sim_list[i]))
-        sim = MC_Simulation(sim_list[i], out_list[i])
-        sims.append(sim)
-        if (sim.sizeL, sim.res) in sim_dat.keys():
-            sim_dat[(sim.sizeL, sim.res)].append(sim.norm)
-        else:
-            sim_dat[(sim.sizeL, sim.res)] = [sim.norm]
+        for i in range(0,len(sim_list)):
+            print(' -> %s'%(sim_list[i]))
+            f.write(' -> %s\n'%(sim_list[i]))
+            sim = MC_Simulation(sim_list[i], out_list[i])
+            sims.append(sim)
+            if (sim.sizeL, sim.res) in sim_dat.keys():
+                sim_dat[(sim.sizeL, sim.res)].append(sim.norm)
+            else:
+                sim_dat[(sim.sizeL, sim.res)] = [sim.norm]
 
-    print('\nobtaining norms:')
-    for param in sim_dat.keys():
-        mu = np.mean(sim_dat[param])
-        sigm = np.std(sim_dat[param])
+        print('\nobtaining norms:')
+        f.write('\nnorms found for individual sets:\n')
+        norms_per_set = []
+        norms_per_sim = []
+        for param in sim_dat.keys():
+            
+            mu = np.mean(sim_dat[param])
+            sigm = np.std(sim_dat[param])
 
-        print('L=%d, N=%d: norm = %f +- %f' %(param[0], param[1], mu, sigm))
+            print('L=%d, N=%d: norm = %f +- %f' %(param[0], param[1], mu, sigm))
+            f.write('L=%d, N=%d: norm = %f +- %f\n' %(param[0], param[1], mu, sigm))
+            norms_per_set.append([param, mu, sigm])
+            norms_per_sim += sim_dat[param]
 
-    norm = 0
+        norm = np.mean(norms_per_sim)
+        sig= np.std(norms_per_sim)
+        
+        f.write('\ncombined result from all files: norm = %f +- %f' %(norm, sig))
+                    
     return sims, norm
+
+
